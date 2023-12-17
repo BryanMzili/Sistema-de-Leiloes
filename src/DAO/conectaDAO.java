4a37c24572184070e2f80ac9f34938afb646ff45
Bryan Lemes <bryansilvalemes@gmail.com>
2023-12-17 10:46:47 -0300

    Configuração Conexão com o banco

--- src/DAO/conectaDAO.java
+++ src/DAO/conectaDAO.java
@@ -7,15 +7,18 @@ import javax.swing.JOptionPane;
 
 public class conectaDAO {
 
-    public Connection connectDB() {
+    public static Connection connectDB() {
         Connection conn = null;
 
         try {
-
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-
+            
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=user_leilao&password=12345");
+            JOptionPane.showMessageDialog(null, "Conexão foi um sucesso!!!");
+            
         } catch (SQLException erro) {
+            
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            
         }
         return conn;
     }
